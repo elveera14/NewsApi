1ecb9c074959448721974614e360954b9094b76d
elveera14 <53492991+elveera14@users.noreply.github.com>
2021-03-30 22:53:25 +0530

    [IMP] [HD]: Added comments in main.py
    Added comments on all method calls describing the purpose of the same

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ def main():
     newsapi_obj = NewsApi()
     newsapi_obj._key = '0eacfb62485f4a4cacf8b62680452532'
     
+    #To fetch top headlines from newsapi
     #possible display_items - ['name', 'author', 'title', 'description', 'publishedAt']
     TopHeadlines().show_top_headlines(display_items=['name', 'author', 'title', 'description', 'publishedAt'])
     
+    #To fetch sources from newsapi
     #possible display_items - ['name', 'description', 'url', 'category', 'language', 'country']
     Sources().show_sources(display_items=['name', 'description', 'url', 'category', 'language', 'country'])
 
+    #To fetch articles based on the given search item
     #possible display_items - ['name', 'author', 'title', 'description', 'publishedAt']
-    Everything().show_everything(kw_to_search_in_title='narendra modi', kw_to_serch_in_both=False, display_items=['name', 'author', 'title', 'description', 'publishedAt'])
+    Everything().show_everything(kw_to_search_in_title='airtel', kw_to_serch_in_both=False, display_items=['name', 'author', 'title', 'description', 'publishedAt'])
 
 main()
