29d687559bbecf08e33c99406e9088676ecaa8bb
elveera14 <53492991+elveera14@users.noreply.github.com>
2021-03-30 22:36:32 +0530

    [IMP][HD]: Added comments in everything.py
    Added comments in all the methods and class

--- everything.py
+++ everything.py
@@ -1,10 +1,13 @@
 from newsapi import NewsApi
 
 class Everything(NewsApi):
+    '''This class handles request and response with the newsapi on 'everything' endpoint.'''
 
 
     def fetch_everything(self, kw_to_search_in_title=False, kw_to_serch_in_both=False, sources=False, 
                 pagesize=10, page=1):
+        '''This method fetches data as per the given parameters from the 'everything' endpoint of the
+        newsapi.'''
         params = {}
         if kw_to_search_in_title:
             params.update({'qInTitle' : kw_to_search_in_title})
@@ -18,6 +21,8 @@ class Everything(NewsApi):
 
     def show_everything(self, kw_to_search_in_title='us', kw_to_serch_in_both=False, sources=False, pagesize=10,
                                       page=1, display_items=['name', 'title']):
+        '''This method prints data fetched from 'everything' endpoint of the newsapi in a visually appealing 
+        manner.'''
         result = self.fetch_everything(kw_to_search_in_title, kw_to_serch_in_both, sources, pagesize, page)
         articles = result.get('articles')
         for count, article in enumerate(articles):
