5d77f5ab4e612ffc77e94ba1a0439264d1c84e97
elveera14 <53492991+elveera14@users.noreply.github.com>
2021-03-30 22:44:53 +0530

    [IMP][HD]: Added comments in sources.py
    Added comments in all the methods and class.

--- sources.py
+++ sources.py
@@ -1,9 +1,12 @@
 from newsapi import NewsApi
 
 class Sources(NewsApi):
+    '''This class handles request and response with the newsapi on 'sources' endpoint.'''
 
 
     def fetch_sources(self, country='us', category=False, language=False, pagesize=10, page=1):
+        '''This method fetches data as per the given parameters from the 'sources' endpoint of the
+        newsapi.'''
         params = {}
         if country:
             params.update({'country' : country})
@@ -17,6 +20,8 @@ class Sources(NewsApi):
 
     def show_sources(self, country='us', category=False, language=False, pagesize=10,
                                       page=1, display_items=['name', 'category']):
+        '''This method prints data fetched from 'sources' endpoint of the newsapi in a visually
+        appealing manner.'''
         result = self.fetch_sources(country, category, language, pagesize, page)
         sources = result.get('sources')
         for count, article in enumerate(sources):
